511e43ed6666f4f5bd5055479d62e6d7ea186f7d
bhagened <bhageneder@ycp.edu>
2022-11-05 05:11:56 -0400

    End of Day 1
    Completed some rudimentary methods
    Trying to test the response of the API
    Setting up database installation.

--- main/API_Suite/productSearchAPI.py
+++ main/API_Suite/productSearchAPI.py
@@ -1,14 +1,26 @@
 import requests
+import time
 
 url = "https://target-com-shopping-api.p.rapidapi.com/product_search"
+rapidKey = '930401d6a5mshe61534d11163938p1ecd97jsn6a9d82d02d3d'
 
-querystring = {"store_id":"1122","keyword":"womens shoes","offset":"0","count":"25"}
+def productSearch(store_id, keyword, offset, count):
+	inputquerystring = {"store_id":store_id,"keyword":keyword,"offset":offset,"count":count}
+	querystring = {"store_id":"1122","keyword":"womens shoes","offset":"0","count":"1"}
 
-headers = {
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-	"X-RapidAPI-Host": "target-com-shopping-api.p.rapidapi.com"
-}
+	headers = {
+		"X-RapidAPI-Key": rapidKey,
+		"X-RapidAPI-Host": "target-com-shopping-api.p.rapidapi.com"
+	}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+	inputresponse = requests.request("GET", url, headers=headers, params=inputquerystring)
+	#expectedresponse = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+	#print("Actual Response: {}\n".format(inputresponse.text))
+
+
+	#print("Expected Response: {}\n".format(expectedresponse.text[1:25]))
+
+	return inputresponse
+
+productSearch("1122","womens shoes","0","1")
--- main/dataStorage_Suite/updateJson.py
+++ main/dataStorage_Suite/updateJson.py
@@ -1,5 +1,22 @@
 import json
+ 
+#Initialize dict object to store json data
+dictionary = {}
 
+#Method to determine if a list is empty
+def isEmpty(array):
+    if not array:
+        return True
+    else:
+        return False
 
-def obtain_json(json_name):
+ 
+def obtain_json(json_name, data_to_be_written):
+    while(not data_to_be_written.isEmpty()):
+        dictionary.update()
     
+
+
+    
+with open("sample.json", "w") as outfile:
+    json.dump(dictionary, outfile)
